7fb191ed453eeb5d33e6a4f2526e0183a2c7c54e
Natarajan M <87933421+Natarajan2801@users.noreply.github.com>
2024-05-18 21:41:38 +0530

    Update Naukuri.java

--- src/test/java/test/Naukuri.java
+++ src/test/java/test/Naukuri.java
@@ -129,7 +129,7 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class NonHeadlessTest {
+public class Naukuri {
 
     private WebDriver driver;
 
