dbee58c6b4ed86999fac1ca9c1007f13c66d15dc
Natarajan M <87933421+Natarajan2801@users.noreply.github.com>
2024-05-18 14:30:34 +0530

    Update Naukuri.java

--- src/test/java/test/Naukuri.java
+++ src/test/java/test/Naukuri.java
@@ -31,9 +31,9 @@ public class Naukuri {
 	public void setUp() {
 		WebDriverManager.chromedriver().setup();
 		ChromeOptions options = new ChromeOptions();
-//		options.addArguments("--no-sandbox");
-//		options.addArguments("--disable-dev-shm-usage");
-//		options.addArguments("--headless");
+		options.addArguments("--no-sandbox");
+		options.addArguments("--disable-dev-shm-usage");
+		options.addArguments("--headless");
 		driver = new ChromeDriver(options);
 		driver.navigate().to("https://www.naukri.com/nlogin/login");
 		driver.manage().window().maximize();
